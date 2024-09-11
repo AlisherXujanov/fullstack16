930f60a79a18c9a872c9b58a305c1474163050ac
Alisher <test@gmail.com>
2024-09-11 18:56:06 +0500

    update

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -2,8 +2,20 @@ import "./style.scss"
 import Heading from "../common/Heading"
 import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 import LatestProducts from "../Home/LatestProducts"
+import { useState } from "react";
 
 function Products(props) {
+    const [sectionName, setSectionName] = useState("products")
+
+    function activateSection(e) {
+        const { name } = e.target
+
+        const btns = document.querySelectorAll(".settings-wrapper .action-buttons button")
+        btns.forEach(btn => { btn.classList.remove("active") })
+        e.target.classList.add("active")
+    }
+
+
     return (
         <div className="products-page-wrapper">
             <Heading title="Products" path="Products">
