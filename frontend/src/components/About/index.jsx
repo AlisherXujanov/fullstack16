991d9ad3886b94158df33d739e331e595998038b
Alisher <alisherxujanov163@gmail.com>
2024-09-25 18:00:48 +0500

    small fix

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -4,8 +4,6 @@ import { BsInfoSquareFill } from "react-icons/bs";
 import Contact from './Contact';
 import ShopexProducts from "../Home/ShopexProducts";
 import Reviews from "./Reviews";
-import Test from "./Test.jsx"
-import UseContextTest from "./UseContextTest.jsx"
 import { useContext } from "react";
 import { context } from "../../store";
 
