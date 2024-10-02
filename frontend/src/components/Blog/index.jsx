378bedb0eb87206a57a55350f7fa5bf053901257
Alisher <alisherxujanov163@gmail.com>
2024-10-02 19:08:49 +0500

    update

--- frontend/src/components/Blog/index.jsx
+++ frontend/src/components/Blog/index.jsx
@@ -1,13 +1,26 @@
 import "./style.scss"
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { context } from "../../store"
 
 function Blog() {
     const { state, dispatch } = useContext(context)
+    const [counter, setCounter] = useState(0)
+
+    useEffect(() => {
+        console.log("Counter changed to ", counter)
+    })
+
 
     return (
         <div align='center'>
             <h1>Blog</h1>
+
+            <hr />
+            <div className="container">
+                <button className="delete-btn" onClick={(e) => setCounter(counter-1)}>Decrement</button>
+                <h2>{counter}</h2>
+                <button className="cancel-btn" onClick={(e) => setCounter(counter+1)}>Increment</button>
+            </div>
         </div>
     )
 }
