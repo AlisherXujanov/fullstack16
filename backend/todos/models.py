669e20c9d840b1a92da38c453e93ec68197ada48
Alisher <alisherxujanov163@gmail.com>
2025-01-06 22:38:17 +0500

    updatge

---
+++ backend/todos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+# Заметки
+class Todos(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    
+    class Meta:
+        verbose_name_plural = 'Todos'
+    
