6f89ce87ec62562080ede5d6d213524d39466c93
Alisher <alisherxujanov163@gmail.com>
2025-01-24 21:24:03 +0500

    update

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -3,52 +3,20 @@ from .models import Todos
 from .forms import TodoForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.views.generic import TemplateView, ListView, DetailView
-
-
-# HTTP methods of request  =>  HTTP-методы запроса
-# GET    => получение данных
-# POST   => отправка данных
-# PUT    => обновление данных
-# PATCH  => обновление части данных
-# DELETE => удаление данных
-
-
-# def home_page(request):  # запрос
-#     # for now, the view works in only GET method requests
-#     # RU: пока что, view работает только с GET-запросами
-#     context = {
-#         "todos": Todos.objects.all()
-#     }
-#     return render(request, 'home.html', context)
-
-
-# class HomePageView(TemplateView):
-#     template_name = 'home.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['todos'] = Todos.objects.all()
-#         return context
-
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView )
 
 class HomePageView(ListView):
     model = Todos
     template_name = 'home.html'
     context_object_name = 'todos'
 
-
-# API  => Application Programming Interface
-
-
 def about(request):
     return render(request, 'about.html')
-
-
-# def todo_details(request, pk: int):
-#     # pk == id
-#     todo = Todos.objects.get(id=pk)
-#     return render(request, 'todo_details.html', {'todo': todo})
 
 
 class TodoDetailsView(DetailView):
@@ -57,27 +25,36 @@ class TodoDetailsView(DetailView):
     context_object_name = 'todo'
 
 
+class CreateTodoView(CreateView):
+    model = Todos
+    form_class = TodoForm  # This lets to use the form that we created inside templates
+    template_name = 'create_todo.html'
+    success_url = '/'
 
-def create_todo(request):
-    if request.method == "POST":
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            obj = form.save(commit=False)
-            admin = User.objects.first()
-            obj.author = admin
-            obj.save()
-            messages.success(request, "Todo added successfully")
-            return redirect('home')
-
-    context = {"form": TodoForm()}
-    return render(request, 'create_todo.html', context=context)
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        messages.success(self.request, "Todo added successfully")
+        return super().form_valid(form)
 
 
-def delete_todo(request, pk):
-    todo = Todos.objects.get(id=pk)
-    todo.delete()
-    messages.success(request, "Todo deleted successfully")
-    return redirect('home')
+# def delete_todo(request, pk):
+#     todo = Todos.objects.get(id=pk)
+#     todo.delete()
+#     messages.success(request, "Todo deleted successfully")
+#     return redirect('home')
+
+
+class DeleteTodoView(DeleteView):
+    model = Todos
+    template_name = 'delete_todo.html'
+    success_url = '/'
+    context_object_name = 'todo'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Todo deleted successfully")
+        return super().delete(request, *args, **kwargs)
 
 
 def update_todo(request, pk):
