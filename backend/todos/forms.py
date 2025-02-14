a93a648e466336d3ea6d250b1ae67ef6f12d3726
Alisher <alisherxujanov163@gmail.com>
2025-02-14 21:00:35 +0500

    deleted

--- backend/todos/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Todos
-
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model = Todos
-        fields = ('title', 'description')
--- backend/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import Group, User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
